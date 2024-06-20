ec6e37819d72d73a828cd33ba69df3932a7958d1
zadohu22 <zadohu22@gmail.com>
2024-06-19 20:42:28 -0500

    starts ai integration

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import {
-	EnhancedGenerateContentResponse,
-	GoogleGenerativeAI,
-} from '@google/generative-ai';
+  EnhancedGenerateContentResponse,
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
 
 dotenv.config();
 
@@ -12,12 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY as string);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const corsOptions = {
-	origin: '*',
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
@@ -25,19 +25,33 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 const testQuery = async (
-	prompt: string
+  prompt: string
 ): Promise<EnhancedGenerateContentResponse> => {
-	const result = await model.generateContent(prompt);
-	const response = result.response;
-	return response;
+  const result = await model.generateContent(prompt);
+  const response = result.response;
+  return response;
 };
 
 // Basic route
-app.get('/', (req: Request, res: Response) => {
-	res.send('Hello, TypeScript with Express!');
+app.get("/", async (req: Request, res: Response) => {
+  const input = req.body;
+  try {
+    if (!input) {
+      res.status(400).send("Prompt query parameter is required");
+      return;
+    }
+
+    // Call the testQuery function with the extracted prompt
+    const response = await testQuery(input);
+
+    // Send the result back to the client
+    res.json(response);
+  } catch (error) {
+    res.status(500).send("An error occurred while processing your request");
+  }
 });
 
 // Start the server
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
